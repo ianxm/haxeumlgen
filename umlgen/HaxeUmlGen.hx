0205d6cf7cc1eff7bff59bb0f41ad9f96a3fd6df
ian <ian@ian-desktop.(none)>
2009-01-24 13:07:07 -0500

    set outdir from cmdline

--- umlgen/HaxeUmlGen.hx
+++ umlgen/HaxeUmlGen.hx
@@ -11,8 +11,8 @@ class HaxeUmlGen
   /** input filename (xml file from haxe compiler) **/
   private var inFname : String;
 
-  /** output filename (png file) **/
-  private var outFname : String;
+  /** output directory. if not set, same as input file  **/
+  private var outDir : String;
 
   /** target package for diagram **/
   public static var pkg(default,null) : String;
@@ -30,16 +30,31 @@ class HaxeUmlGen
     new HaxeUmlGen().run();
   }
 
+  /**
+	constructor
+   **/
   public function new()
   {
     VERSION = "0.0.1";
+    outDir = null;
   }
 
+  /**
+	this is the main driver.
+   **/
   public function run()
   {
-    parseArgs(); // parse command line arguemnts
-    readXml(); // read input xml file
-    callDot(); // write dot input, call dot
+    try
+    {
+      parseArgs(); // parse command line arguemnts
+      readXml(); // read input xml file
+      callDot(); // write dot input, call dot
+    }
+    catch (ex:String)
+    {
+      neko.Lib.println(ex);
+      neko.Sys.exit(1);
+    }
   }
 
   /**
@@ -49,17 +64,39 @@ class HaxeUmlGen
   {
     var args = neko.Sys.args();
     if( args.length<2 )
-      showHelp();
+      throw ("Not enough arguments");
 
-    for( aa in args )
+    var iter = args.iterator();
+    while( iter.hasNext() )
+    {
+      var aa = iter.next();
       if( aa == args[args.length-2] )
 	inFname = aa;
 
       else if( aa == args[args.length-1] )
 	pkg = aa;
 
+      else if( aa=="-o" )
+	outDir = iter.next();
+      
+      else if( aa.indexOf("--outdir=") != -1 )
+	outDir = aa.substr(9);
+
       else if( aa=="-h" || aa=="--help" )
 	showHelp();
+
+      else
+	throw "Unknown option: " + aa;
+    }
+
+    if( outDir == null )
+    {
+      var indx = inFname.lastIndexOf("/");
+      outDir = (indx == -1) ? "." : inFname.substr(0, indx);
+    }
+
+    if( !neko.FileSystem.exists(outDir) )
+      throw "Error: Output directory doesn't exist";
   }
 
   private function showHelp()
@@ -68,6 +105,7 @@ class HaxeUmlGen
     neko.Lib.println("usage: haxeUmlGen [options] [xml file] [package]");
     neko.Lib.println("");
     neko.Lib.println("options:");
+    neko.Lib.println(" -o --outdir=DIR	change the output directory");
     neko.Lib.println(" -h --help	show this message and exit");
     neko.Sys.exit(0);
   }
@@ -88,13 +126,11 @@ class HaxeUmlGen
   {
     var boxes = dataTypes.filter(function(dd) { return dd.pkg==pkg; });
     if( boxes.isEmpty() )
-    {
-      neko.Lib.println("No classes found in the desired package");
-      neko.Sys.exit(0);
-    }
+      throw "No classes found in the desired package";
 
     // write file
-    var fout = neko.io.File.write(pkg+".dot", false);
+    var dotFname = outDir + "/" + pkg + ".dot";
+    var fout = neko.io.File.write(dotFname, false);
     fout.writeString('digraph uml\n');
     fout.writeString('{\n');
     fout.writeString('        fontname = "Sans";\n');
@@ -109,6 +145,8 @@ class HaxeUmlGen
     fout.close();
 
     // call dot
-    neko.Sys.command('dot -T png -o '+pkg+'.png '+pkg+'.dot');
+    var pngFname = outDir + "/" + pkg + ".png";
+    neko.Sys.command('dot -T png -o ' + pngFname + ' ' + dotFname);
+    neko.FileSystem.deleteFile(dotFname);
   }
 }
